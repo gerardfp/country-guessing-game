9c6316fee458b8431b639719ce225e1819d8bee1
gerardfp <gerardfp@gmail.com>
2022-11-10 08:31:13 +0100

    first commit

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<p>{guessCountry}</p>
+<p>wins {wins} | fails {fails}</p>
+<World on:countryClicked={handleClick} />
+
+<script>
+    import World, { elements, getRandomCountry } from './world.svelte'
+    import { onMount } from 'svelte';
+
+    let guessCountry, wins = 0, fails = 0;
+
+    function handleClick(event) {
+        if(event.detail.country === guessCountry){
+            wins++;
+        } else {
+            fails++;
+        }
+        nextCountry();
+    }
+
+    onMount(() => {
+		nextCountry();
+        console.log(elements);
+    })
+
+    function nextCountry(){
+        guessCountry = getRandomCountry();
+    }
+</script>
+
+<style>
+    p{ text-align: center;}
+</style>
