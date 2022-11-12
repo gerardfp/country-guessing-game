976d389ae719606df6cc0d761c631dbf60c81e1d
gerardfp <gerardfp@gmail.com>
2022-11-12 09:48:26 +0100

    shine

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,20 +1,27 @@
 <p>{guessCountry}</p>
-<p>wins {wins} | fails {fails}</p>
+<p><span class:win class:rewin={!win}> wins {wins}</span> | <span class:fail class:refail={!fail}>fails {fails}</span></p>
+<p class=clicked>{clickedCountry}</p>
 <World on:countryClicked={handleClick} />
 
 <script>
     import { World, getRandomCountry } from 'mapa-mundi'
     import { onMount } from 'svelte';
 
-    let guessCountry, wins = 0, fails = 0;
+    let guessCountry="_", clickedCountry="_", wins = 0, fails = 0, win, fail;
 
     function handleClick(event) {
-        if(event.detail.country === guessCountry){
-            wins++;
-        } else {
-            fails++;
+        if(event.detail.country != null){
+            
+            clickedCountry = event.detail.country;
+            if(event.detail.country === guessCountry){
+                wins++;
+                win=!win;
+            } else {
+                fails++;
+                fail=!fail;
+            }
+            nextCountry();
         }
-        nextCountry();
     }
 
     onMount(() => {
@@ -28,4 +35,28 @@
 
 <style>
     p{ text-align: center;}
+    p.clicked { color: rgb(172, 172, 172)}
+
+    span.rewin { animation: reshine-green 1s; }
+    span.win { animation: shine-green 1s; }
+    span.refail { animation: reshine-red 1s; }
+    span.fail { animation: shine-red 1s; }
+
+
+    @keyframes shine-green {
+        30%{ text-shadow: 0 0 16px rgb(56, 141, 56), 0 0 12px rgb(17, 189, 17), 0 0 6px rgb(0, 255, 0);}
+        100%{ text-shadow: none;}
+    }
+    @keyframes reshine-green {
+        30%{ text-shadow: 0 0 16px rgb(56, 141, 56), 0 0 12px rgb(17, 189, 17), 0 0 6px rgb(0, 255, 0);}
+        100%{ text-shadow: none;}
+    }
+    @keyframes shine-red {
+        30%{text-shadow: 0 0 16px rgb(141, 56, 56), 0 0 12px rgb(189, 17, 17), 0 0 6px rgb(255, 0, 0);}
+        100%{ text-shadow: none;}
+    }
+    @keyframes reshine-red {
+        30%{text-shadow: 0 0 16px rgb(141, 56, 56), 0 0 12px rgb(189, 17, 17), 0 0 6px rgb(255, 0, 0);}
+        100%{ text-shadow: none;}
+    }
 </style>
