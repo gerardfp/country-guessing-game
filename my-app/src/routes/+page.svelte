cbe208abc496048742c8a85ed61bd17214a4d285
gerardfp <gerardfp@gmail.com>
2022-11-17 15:00:03 +0100

    list

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -2,12 +2,24 @@
 <p><span class:win class:rewin={!win}> wins {wins}</span> | <span class:fail class:refail={!fail}>fails {fails}</span></p>
 <p class=clicked>{clickedCountry}</p>
 <World on:countryClicked={handleClick} />
+<ul class="guessed">
+    {#each [...guessed] as country}
+        <li>{country}</li>
+    {/each}
+</ul>
+<ul class="failed">
+    {#each [...failed] as country}
+        <li>{country}</li>
+    {/each}
+</ul>
 
 <script>
-    import { World, getRandomCountry } from 'mapa-mundi'
+    import { World,countries } from 'mapa-mundi'
     import { onMount } from 'svelte';
     let guessCountry="_", clickedCountry="_", wins = 0, fails = 0, win, fail;
+    let guessed = new Set(), failed = new Set();
 
+    //let countries =["Spain","France","Italy","Germany"];
     function handleClick(event) {
         if(event.detail.country != null){
             
@@ -15,10 +27,15 @@
             if(event.detail.country === guessCountry){
                 wins++;
                 win=!win;
+                guessed.add(guessCountry);
+                failed.delete(guessCountry);
+                guessed = guessed;
             } else {
                 fails++;
                 fail=!fail;
+                failed.add(guessCountry);
             }
+            failed = failed;
             nextCountry();
         }
     }
@@ -30,11 +47,46 @@
     function nextCountry(){
         guessCountry = getRandomCountry();
     }
+
+    function getRandomCountry(){
+        let i = Math.floor(Math.random()*countries.length);
+        let starti = i;
+        while(true){
+            if (guessed.has(countries[i])){
+                i++;
+                if (i>=countries.length) i=0;
+            } else {
+                return countries[i];
+            }
+            if (i==starti) {
+                return "Congratulations, you guessed them all!";
+            }
+        }
+    }
 </script>
 
 <style>
     p{ text-align: center;}
     p.clicked { color: rgb(172, 172, 172)}
+
+    ul {
+        list-style-type: none;
+    }
+    ul.guessed {
+        color: green;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+    ul.failed {
+        color: red;
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding-right: 40px;
+    }
+
+
 
     span.rewin { animation: reshine-green 1s; }
     span.win { animation: shine-green 1s; }
